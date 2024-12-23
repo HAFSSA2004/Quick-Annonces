62246b30c3fea1e70022cc4121bd18d25841a8ee
hafssa <hafssaelbouhli@gmail.com>
2024-12-23 11:36:22 +0100

    up

--- src/Footer.js
+++ src/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Import Link for routing
 import "./Footer.css"; // Create and link a CSS file for styles
 import { FaTwitter, FaLinkedin, FaFacebook } from "react-icons/fa"; // FontAwesome Icons
 
@@ -11,9 +12,9 @@ function Footer() {
         <div className="footer-section">
           <h4>Company</h4>
           <ul>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Solutions</a></li>
-            <li><a href="#">Insights</a></li>
+            <li><Link to="/about-us">About Us</Link></li>
+            <li><Link to="/solutions">Solutions</Link></li>
+            <li><Link to="/insights">Insights</Link></li>
           </ul>
         </div>
 
@@ -21,12 +22,12 @@ function Footer() {
         <div className="footer-section">
           <h4>Categories</h4>
           <ul>
-            <li><a href="#">Hire Talent</a></li>
-            <li><a href="#">Develop Talent</a></li>
-            <li><a href="#">Gamification Tools</a></li>
-            <li><a href="#">Digital Skills</a></li>
-            <li><a href="#">Commercial Skills</a></li>
-            <li><a href="#">Leadership</a></li>
+            <li><Link to="/hire-talent">Hire Talent</Link></li>
+            <li><Link to="/develop-talent">Develop Talent</Link></li>
+            <li><Link to="/gamification-tools">Gamification Tools</Link></li>
+            <li><Link to="/digital-skills">Digital Skills</Link></li>
+            <li><Link to="/commercial-skills">Commercial Skills</Link></li>
+            <li><Link to="/leadership">Leadership</Link></li>
           </ul>
         </div>
       </div>
@@ -35,16 +36,22 @@ function Footer() {
       <div className="footer-bottom">
         {/* Social Media Links */}
         <div className="social-media">
-          <a href="#"><FaTwitter className="social-icon" /></a>
-          <a href="#"><FaLinkedin className="social-icon" /></a>
-          <a href="#"><FaFacebook className="social-icon" /></a>
+          <a href="javascript:void(0)" className="social-icon">
+            <FaTwitter />
+          </a>
+          <a href="javascript:void(0)" className="social-icon">
+            <FaLinkedin />
+          </a>
+          <a href="javascript:void(0)" className="social-icon">
+            <FaFacebook />
+          </a>
         </div>
 
         {/* Legal Links */}
         <div className="legal-links">
-          <a href="#">Privacy Policy</a>
-          <a href="#">Terms & Conditions</a>
-          <a href="#">Code of Conduct</a>
+          <Link to="/privacy-policy">Privacy Policy</Link>
+          <Link to="/terms-conditions">Terms & Conditions</Link>
+          <Link to="/code-of-conduct">Code of Conduct</Link>
         </div>
       </div>
     </footer>
