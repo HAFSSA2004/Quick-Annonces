88a626e899f3dc4a97b3d10355413c75e0914dfb
hafssa <hafssaelbouhli@gmail.com>
2025-02-27 14:08:58 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ import TechnoPage from './TechnoPage';
 import CarPage from './CarPage';
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
-
+//
 function PrivateRoute({ children }) {
   const { isAuthenticated } = useAuth();
   return isAuthenticated ? children : <Navigate to="/login" replace />;
